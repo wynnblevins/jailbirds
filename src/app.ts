1de434594322617be7dd8c74c6d9377699e0d3b8
wynnblevins <wynnblevins@yahoo.com>
2024-02-23 16:07:16 -0500

    Using node cron-job package to run script at 4 each day

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
+const cron = require('node-cron');
 const {
   findAllJailbirds,
   createJailbird,
@@ -59,13 +60,23 @@ const run = async () => {
     jailbirdsToPost.push(jailbird)
   });
 
-  await postToInsta(jailbirdsToPost);
+  await new Promise(async (resolve) => { 
+    // pausing to try and fool instagram
+    setTimeout(resolve, 15000);
+  });
+
+  await postToInsta(jailbirdsToPost); 
 };
 
-run().then(() => {
-  process.exit(); 
+// Schedule tasks to be run on the server.
+cron.schedule('0 16 * * *', async () => {
+  console.log('Running webscraper.')
+  run().then(() => {
+    console.log('Jailbirds have been updated.');
+  }).catch((err) => {
+    console.error('Encountered error while updating jailbirds. Halting script execution.', err);
+    process.exit();
+  })
 });
-
-
 
 export { Jailbird };
