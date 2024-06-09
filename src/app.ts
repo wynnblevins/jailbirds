a4e7706722394d1d2fbe4c82a67dc3e2c1fb5a51
wynnblevins <wynnblevins@yahoo.com>
2024-06-08 23:03:24 -0400

    Changing deploy time to to 11:30 PM

--- src/app.ts
+++ src/app.ts
@@ -77,7 +77,7 @@ const run = async () => {
 };
 
 
-cron.schedule('30 22 * * *', () => {
+cron.schedule('30 23 * * *', () => {
   run().then(() => {
     console.log('Program complete, stopping execution.');
   }).catch((e) => {
