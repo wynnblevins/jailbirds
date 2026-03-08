598e30a2bb2bc1cd0e7dfcd29209d67f8b09e6fb
Wynn Blevins <wynnblevins@gmail.com>
2026-03-07 19:23:33 -0500

    added cloudinary service

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -22,5 +22,10 @@ module.exports = {
   },
   keys: {
     captchaAPIKey: process.env.CAPTCHA_KEY
+  }, 
+  cloudinary: {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    apiSecret: process.env.CLOUDINARY_API_SECRET
   }
 }
